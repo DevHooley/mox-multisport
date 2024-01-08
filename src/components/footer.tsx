c3c4a503442e25eaebcc8c4beb719fccde00b8df
Your Name <92824573+kyleehooley@users.noreply.github.com>
2024-01-08 12:31:33 -0600

    latest

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-6">
+    <footer className="bg-sky-300 text-white py-6">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
-          <div className="flex-shrink-0"></div>
+          <div className="flex-shrink-0">
+            <h1 className="text-black sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-2xl">
+              Phone number - (312)466-9111
+            </h1>
+          </div>
           <div className="space-x-4">
-            <a href="/home" className="hover:text-gray-300">
+            <a href="/home" className="hover:text-gray-800">
               Home
             </a>
-            <a href="#" className="hover:text-gray-300">
+            <a href="#" className="hover:text-gray-800">
               Terms of Service
             </a>
-            <a href="#" className="hover:text-gray-300">
+            <a href="#" className="hover:text-gray-800">
               Contact Us
             </a>
           </div>
