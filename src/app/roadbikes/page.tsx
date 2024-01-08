d9d6ec65e8031f82000bd6dd8572b23035a4ab71
Your Name <92824573+kyleehooley@users.noreply.github.com>
2024-01-08 15:27:26 -0600

    latest

--- src/app/roadbikes/page.tsx
+++ src/app/roadbikes/page.tsx
@@ -8,20 +8,22 @@ export default function Roadbikes() {
       <section className=" bg-gray-50">
         <main className="flex justify-center items-center p-4">
           <div className="text-center">
-            <h1 className="text-3xl text-black font-bold mb-4">Cervelo Road Bikes</h1>
+            <h1 className="text-3xl text-black font-bold mb-4">
+              Cervelo Road Bikes
+            </h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
               {products.map((product) => (
                 <div
                   key={product.id}
-                  className="mb-8 p-4 border border-gray-300 rounded-md"
+                  className="mb-8 shadow-md bg-white p-4 border border-gray-300 rounded-lg"
                 >
-                    <Image
-                      src={product.image}
-                      alt={product.name}
-                      width={500}
-                      height={300}
-                      layout="responsive"
-                    />
+                  <Image
+                    src={product.image}
+                    alt={product.name}
+                    width={500}
+                    height={300}
+                    layout="responsive"
+                  />
                   <h2 className="text-xl text-black font-semibold mb-2">
                     {product.name}
                   </h2>
@@ -34,8 +36,7 @@ export default function Roadbikes() {
                     Sizes: {product.sizes.join(', ')}
                   </p>
                   {/* Use next/image for images */}
-                  <div className="rounded-md overflow-hidden">
-                  </div>
+                  <div className="rounded-md overflow-hidden"></div>
                 </div>
               ))}
             </div>
