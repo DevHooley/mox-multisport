7f94ec100f2fe9296b75f13e83a68219935777f8
Your Name <92824573+kyleehooley@users.noreply.github.com>
2024-01-09 10:08:23 -0600

    latest

---
+++ src/app/roadbikes/SoloistUltegradi2/page.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image from 'next/image';
+import products from '@/components/roadbikeproductdata';
+
+export default function r5Duraace() {
+  // Filter products to include only the one with id 1
+  const filteredProducts = products.filter((product) => product.id === 3);
+
+  return (
+    <>
+      <section className="bg-gray-50">
+        <main className="flex justify-center items-center p-4">
+          <div className="text-center">
+            <h1 className="text-3xl text-black font-bold mb-4"> Cervelo Soloist Ultegra Di2</h1>
+
+            <div className="grid max-w-[600px] grid-cols-1">
+              {filteredProducts.map((product) => (
+                <div
+                  key={product.id}
+                  className="mb-8 shadow-md bg-white p-4 border border-gray-300 rounded-lg"
+                >
+                  <Image
+                    src={product.image}
+                    alt={product.name}
+                    width={500}
+                    height={300}
+                    style={{ width: 'auto', height: 'auto' }}
+                  />
+                  <h2 className="text-xl text-black font-semibold mb-2">
+                    {product.name}
+                  </h2>
+                  <p className="text-gray-600 mb-2">{product.description}</p>
+                  <p className="text-gray-700 mb-2">{product.about}</p>
+                  <p className="text-green-700 font-bold mb-2">
+                    ${product.price}
+                  </p>
+                  <p className="text-gray-700 mb-2">
+                    Sizes: {product.sizes.join(', ')}
+                  </p>
+                  {/* Use next/image for images */}
+                  <div className="rounded-md overflow-hidden"></div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
+}
--- src/components/roadbikeproductdata.tsx
+++ src/components/roadbikeproductdata.tsx
@@ -20,7 +20,7 @@ const products = [
     sizes: ['48', '51', '54', '56', '58'],
     image:
       '/RoadBikes/S5-DuraAce-Di2-FiveBlk_c0b8d903-a1fb-4d30-bf9b-2cbe8e6e8d05.jpg',
-    url: '/roadbikes/2/page', // Updated URL with product ID
+    url: '/roadbikes/S5Dura-ace-Di2', // Updated URL with product ID
   },
   {
     id: 3,
@@ -31,7 +31,7 @@ const products = [
     price: 6800.0,
     sizes: ['48', '51', '54', '56', '58', '61'],
     image: '/RoadBikes/Soloist-Ult-di2-embers.jpg',
-    url: '/roadbikes/3/page', // Updated URL with product ID
+    url: '/roadbikes/SoloistUltegradi2', // Updated URL with product ID
   },
   {
     id: 4,
@@ -42,7 +42,7 @@ const products = [
     price: 8500.0,
     sizes: ['48', '51', '54', '56', '58', '61'],
     image: '/RoadBikes/Caledonia-5-Ult-Di2-FiveBlack.jpg',
-    url: '/roadbike/4/page', // Updated URL with product ID
+    url: '/roadbikes/Caledonia5', // Updated URL with product ID
   },
   // Add more products as needed
 ];
