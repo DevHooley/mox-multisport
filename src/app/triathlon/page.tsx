d6829cb30b659529d54d43359e40a3b695a0d539
Your Name <92824573+kyleehooley@users.noreply.github.com>
2024-01-17 17:48:46 -0600

    latest

--- src/app/triathlon/page.tsx
+++ src/app/triathlon/page.tsx
@@ -11,7 +11,7 @@ export default function CerveloTriathlon() {
         <main className="flex justify-center items-center p-4">
           <div className="text-center ">
             <h1 className="text-4xl text-slate-700 font-semibold mb-4">
-              Pinarello Road Bikes
+              Cervelo Triathlon Bikes
             </h1>
             <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
               {products.map((product) => (
