a074ec5340f07c02f93bf3090ddcff475fdb87eb
Your Name <92824573+kyleehooley@users.noreply.github.com>
2024-01-31 15:54:07 -0600

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <>
-      <section className="flex h-screen item-center justify-center items-center bg-green-400">
+      <section className="flex h-screen item-center justify-center items-center bg-sky-400">
         Coming soon
       </section>
     </>
